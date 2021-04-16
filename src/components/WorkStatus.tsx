c672b253cac3829a51aa0a14849e56abc92cdf8d
100pecheneK <mistermihail23@gmail.com>
2021-04-16 12:35:02 +0500

    fix workstatus

--- src/components/WorkStatus.tsx
+++ src/components/WorkStatus.tsx
@@ -2,20 +2,25 @@ import React, { useRef } from 'react'
 import getWorkStatus from '@utils/getWorkStatus'
 import { TimeTableType } from 'selectors/selectors'
 
-export default function WorkStatus({ timetable }: {timetable: TimeTableType}) {
-  const workStatus = useRef(getWorkStatus(timetable))
+export default function WorkStatus({
+  timetable,
+}: {
+  timetable: TimeTableType
+}) {
+  const workStatus = getWorkStatus(timetable)
+  console.log("workStatus", workStatus)
 
   return (
     <>
       <b
         className={`mr-2 ${
-          workStatus.current.isOpen ? 'text-green-600' : 'text-red-600'
+          workStatus.isOpen ? 'text-green-600' : 'text-red-600'
         }`}
       >
-        {workStatus.current.isOpen ? 'Открыто' : 'Закрыто'}
+        {workStatus.isOpen ? 'Открыто' : 'Закрыто'}
       </b>
-      {workStatus.current.isOpen ? 'Закроется' : 'Откроется'} в&nbsp;
-      {workStatus.current.work}
+      {workStatus.isOpen ? 'Закроется' : 'Откроется'} в&nbsp;
+      {workStatus.work}
     </>
   )
 }
