a7c1aca63b54f2397482cf73405b986ab008c195
100pecheneK <mistermihail23@gmail.com>
2021-04-16 13:17:46 +0500

    fix

--- src/components/WorkStatus.tsx
+++ src/components/WorkStatus.tsx
@@ -1,5 +1,5 @@
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import getWorkStatus from '@utils/getWorkStatus'
-import React from 'react'
 import { TimeTableType } from 'selectors/selectors'
 
 export default function WorkStatus({
@@ -8,16 +8,18 @@ export default function WorkStatus({
   timetable: TimeTableType
 }) {
   const workStatus = getWorkStatus(timetable)
+  const [statusColor, setStatusColor] = useState('')
+  useEffect(() => {
+    setStatusColor(['text-red-600', 'text-green-600'][+workStatus.isOpen])
+  }, [workStatus])
+  if (!statusColor) return null
   return (
-    <p>
-      <b
-        className='mr-2'
-        style={{ color: workStatus.isOpen ? '#07966a' : '#dc2626' }}
-      >
+    <>
+      <b className={`mr-2 ${statusColor}`}>
         {workStatus.isOpen ? 'Открыто' : 'Закрыто'}
       </b>
       {workStatus.isOpen ? 'Закроется' : 'Откроется'} в&nbsp;
       {workStatus.work}
-    </p>
+    </>
   )
 }
