9c358c005c3d623db61cb9b3cf5239a4200f6445
100pecheneK <mistermihail23@gmail.com>
2021-05-05 19:36:14 +0500

    add config in app.js

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const nodemailer = require('nodemailer')
+const { google } = require('googleapis')
+
+const CLIENT_ID =
+  '932993602522-jo89jsohmefkl3o57ph5id33jll6ughk.apps.googleusercontent.com'
+const CLIENT_SECRET = 'pvf7B1LCf1jjE9Ik8CeyTviU'
+const REQUEST_URI = 'https://developers.google.com/oauthplayground'
+const REFRESH_TOKEN =
+  '1//04ZAuA3J_3tn2CgYIARAAGAQSNwF-L9Ir-3JKec3LQx7nwCOPzCtslLeqW_bmJUe1DR8ufhuwCZw3xgZQOLcqLDVQUj_h0Bg-020'
