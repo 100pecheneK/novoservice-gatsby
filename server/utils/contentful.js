8cee8b6294752189cdda5b67779690d17ba6fd3e
100pecheneK <mistermihail23@gmail.com>
2021-05-22 13:15:53 +0500

    add attachments in email

--- server/utils/contentful.js
+++ server/utils/contentful.js
@@ -2,23 +2,37 @@ require('dotenv').config()
 const { createClient } = require('contentful')
 
 /**
+ *
+ * @returns
+ */
+function getContentfulClient() {
+  return createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  })
+}
+
+/**
  * API
  * content_type: services
  * serviceName: photonovik
  * fields.serviceSettings.fields.email
  */
 async function getSettingsFromContentful() {
-  const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-  })
-  const res = await client.getEntries({ content_type: 'services' })
+  const client = getContentfulClient()
+  const services = await client.getEntries({ content_type: 'services' })
 
-  const { email } = res.items.find(
+  const { email } = services.items.find(
     item => item.fields.serviceName === 'photonovik'
   ).fields.serviceSettings.fields
 
   return { email }
+}
+
+async function getProductFromContentfulById(id) {
+  const client = getContentfulClient()
+  const product = await client.getEntry(id)
+  return product
 }
 
 module.exports = getSettingsFromContentful
